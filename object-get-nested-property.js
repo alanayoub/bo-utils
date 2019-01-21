7d32b2bde80e89c6aa2137f92fae6656c4004355
Alan Ayoub <alanayoub@gmail.com>
2019-01-21 11:41:19 +0000

    adding time-ago and renameing object-get-nested-property

--- object-get-nested-property.js
+++ object-get-nested-property.js
@@ -7,7 +7,7 @@
  * @return {Object|String} return the property, can be any value
  *
  */
-module.exports = function objectGetNestedProperty(obj, str) {
+export default function objectGetNestedProperty(obj, str) {
   const pathArr = str.split('.');
   return pathArr.reduce((o, k) => (o && o[k] !== void 0) ? o[k] : void 0, obj);
 }
---
+++ time-ago.js
@@ -0,0 +1,21 @@
+/**
+ *
+ * Timeago
+ *
+ * NOTE: Requires moment
+ *
+ * @param {String|Date|Number} timestamp
+ * @return {String} e.g. 'a few seconds'
+ *
+ */
+export default function timeago(timestamp) {
+  let timeago;
+  if (timestamp !== void 0) {
+    const startDate = moment(timestamp);
+    const endDate = moment(new Date);
+    const ms = Math.abs(startDate.diff(endDate));
+    const duration = moment.duration(ms).humanize();
+    timeago = `${duration} (${ms / 1000}s)`;
+  }
+  return timeago;
+}
