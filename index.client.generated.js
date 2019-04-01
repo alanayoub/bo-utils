64a25c55b0c8fa302f74c698ad98d71232b05f2e
Alan Ayoub <alanayoub@gmail.com>
2019-04-01 12:43:28 +0100

    including generated files so we dont need to build anything on the server

---
+++ index.client.generated.js
@@ -0,0 +1,71 @@
+//
+// This is a generated file
+//
+
+import arrayAverage from './src/array-average.js';
+import arrayNormalize from './src/array-normalize.js';
+import arrayPercentRange from './src/array-percent-range.js';
+import arrayToObject from './src/array-to-object.js';
+import checkDataTypesEqual from './src/check-data-types-equal.js';
+import createSvgInMemoryNode from './src/create-svg-in-memory-node.js';
+import d3SimpleBarChart from './src/d3-simple-bar-chart.js';
+import d3Sparkline from './src/d3-sparkline.js';
+import diffNumericStrings from './src/diff-numeric-strings.js';
+import formatNumberAsCurrency from './src/format-number-as-currency.js';
+import formatNumberAs from './src/format-number-as.js';
+import getRandomInt from './src/get-random-int.js';
+import htmlPollElement from './src/html-poll-element.js';
+import htmlToggleClass from './src/html-toggle-class.js';
+import isFloat from './src/is-float.js';
+import isInteger from './src/is-integer.js';
+import isNumber from './src/is-number.js';
+import numberGroupDigits from './src/number-group-digits.js';
+import numberNormalize from './src/number-normalize.js';
+import numberScale from './src/number-scale.js';
+import objectGetNestedProperty from './src/object-get-nested-property.js';
+import objectIsEmptyObject from './src/object-is-empty-object.js';
+import objectIsObject from './src/object-is-object.js';
+import objectToArray from './src/object-to-array.js';
+import partialApplication from './src/partial-application.js';
+import stringCamelcase from './src/string-camelcase.js';
+import stringCapitalizeFirstLetter from './src/string-capitalize-first-letter.js';
+import timeAgo from './src/time-ago.js';
+import timeseriesPrune from './src/timeseries-prune.js';
+import timeseriesRescale from './src/timeseries-rescale.js';
+import timeseriesScale from './src/timeseries-scale.js';
+import timeseriesThin from './src/timeseries-thin.js';
+
+export {
+  arrayAverage,
+  arrayNormalize,
+  arrayPercentRange,
+  arrayToObject,
+  checkDataTypesEqual,
+  createSvgInMemoryNode,
+  d3SimpleBarChart,
+  d3Sparkline,
+  diffNumericStrings,
+  formatNumberAsCurrency,
+  formatNumberAs,
+  getRandomInt,
+  htmlPollElement,
+  htmlToggleClass,
+  isFloat,
+  isInteger,
+  isNumber,
+  numberGroupDigits,
+  numberNormalize,
+  numberScale,
+  objectGetNestedProperty,
+  objectIsEmptyObject,
+  objectIsObject,
+  objectToArray,
+  partialApplication,
+  stringCamelcase,
+  stringCapitalizeFirstLetter,
+  timeAgo,
+  timeseriesPrune,
+  timeseriesRescale,
+  timeseriesScale,
+  timeseriesThin
+}
